b278c5f7d1a13ffca083af3f61b3c00ae2b06637
Mylene Jacquemart <mjacqu@gmail.com>
2019-09-18 10:06:15 -0600

    added path to file with all EQ within 1500km

--- FC_Seismicity.py
+++ FC_Seismicity.py
@@ -8,10 +8,11 @@ from datetime import datetime
 '''
 Import seismicity dataset (csv file, downloaded from https://earthquake.usgs.gov/earthquakes/search/)
 Calculate distances to Flat Creek
-Plot distance vs time with magnitudes for different EQ. 
+Plot distance vs time with magnitudes for different EQ.
 '''
 
-in_file = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/seismicity/FC_EQcatalog.csv'
+#in_file = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/seismicity/FC_EQcatalog.csv'
+in_file = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/seismicity/Mag4-8Max1000km.csv'
 data = pd.read_csv(in_file, parse_dates=['time'])
 #data.pytime = [x.to_pydatetime() for x in data.time]
 
@@ -42,8 +43,8 @@ plt.show()
 #plt.savefig('AllSeismicity.png')
 
 #Plot zoomed in versions of above plot
-zoom_start = datetime.strptime('2013-07-31', '%Y-%m-%d')
-zoom_end = datetime.strptime('2013-08-06', '%Y-%m-%d')
+zoom_start = datetime.strptime('2015-07-31', '%Y-%m-%d')
+zoom_end = datetime.strptime('2015-08-06', '%Y-%m-%d')
 
 fig, ax = plt.subplots(figsize=(15,7))
 ax.scatter(list(data.time), data.distance, s = data.mag, alpha = 0, label = 'Magnitude')
@@ -51,7 +52,7 @@ ax.scatter(list(data.time.loc[data.mag < 2]), data.distance.loc[data.mag < 2], s
 ax.scatter(list(data.time.loc[(data.mag >=2) & (data.mag < 3)]), data.distance.loc[(data.mag >=2) & (data.mag < 3)], color = 'orange', s = 30*data.mag.loc[(data.mag >=2) & (data.mag < 3)], alpha = 1, linewidths = 2, edgecolors = 'k', label = '2<= M < 3')
 ax.scatter(list(data.time.loc[(data.mag >=3) & (data.mag < 4)]), data.distance.loc[(data.mag >=3) & (data.mag < 4)], color = 'red', s = 50*data.mag.loc[(data.mag >=3) & (data.mag < 4)], alpha = 1, linewidths = 2, edgecolors = 'k', label = '3<= M < 4')
 ax.scatter(list(data.time.loc[data.mag > 4]), data.distance.loc[data.mag > 4], s = 70*data.mag.loc[data.mag > 4], color = 'purple', alpha = 1, linewidths = 2, edgecolors = 'k', label = 'M > 4')
-ax.axvline(datetime.strptime('2013-08-05 18:59:36', '%Y-%m-%d %H:%M:%S'), color='black')
+ax.axvline(datetime.strptime('2015-07-31 18:59:36', '%Y-%m-%d %H:%M:%S'), color='black')
 ax.set_xlim([zoom_start, zoom_end])
 plt.legend(fontsize = 14)
 plt.xlabel('Time [UTC]', fontsize = 16)

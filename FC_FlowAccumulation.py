dba77e8456f7160a2abcbc8ab5df8441ebd0abf1
Mylene Jacquemart <mjacqu@gmail.com>
2019-10-25 09:48:11 -0600

    added DEM depresion filling step

--- FC_FlowAccumulation.py
+++ FC_FlowAccumulation.py
@@ -2,18 +2,23 @@ import richdem
 import matplotlib.pyplot as plot
 
 
-infile = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/VolumeCalculations/NewProcessingSummer2018/2012_EPSG3413_crop.tif'
+infile = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/Hydrology/ifsar_merged.tif'
 
 rd = richdem.LoadGDAL(infile, no_data = -9999)
 
+filled = richdem.FillDepressions(rd, in_place=False)
 flow_acc = richdem.FlowAccumulation(rd, method = 'Rho8')
 
+
 fig, (ax1, ax2) = plt.subplots(1,2)
-ax1.imshow(flow_acc, interpolation='none', vmin = 1, vmax = 20)
+ax1.imshow(flow_acc, interpolation='none', vmin = 40, vmax = 100)
 ax2.imshow(rd, interpolation='none', vmin = 2000, vmax = 3000)
-ax1.set_xlim([1250,1800])
-ax1.set_ylim([1400,900])
-ax2.set_xlim([1250,1800])
-ax2.set_ylim([1400,900])
-#plt.colorbar()
+ax1.set_xlim([2100,3000])
+ax1.set_ylim([3300,2800])
+ax2.set_xlim([2100,3000])
+ax2.set_ylim([3300,2800])
+#ax1.colorbar()
+#ax2.colorbar()
 plt.show()
+
+richdem.SaveGDAL("/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/Hydrology/Rho8_filled.tif", flow_acc)

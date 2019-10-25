f94c0a977a05c505a51b3c29c3463f2d026210d1
Mylene Jacquemart <mjacqu@gmail.com>
2019-10-25 09:47:03 -0600

    changed color scheme, added decadal mean

--- FC_GeologyFigure4.py
+++ FC_GeologyFigure4.py
@@ -8,8 +8,18 @@ fc_all = pd.read_csv('/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/met
 
 #fc_all.plot(x='time', y=["2100",'2200','2300','2400', "2500", '2600', '2700'], figsize = (12,6),
 #                colormap = 'gist_earth', style = '--.', linewidth = 1, markersize =4)
+mean80s = fc_all['2100'][(fc_all['time'] > 1979) & (fc_all['time'] < 1990)].mean()
+mean90s = fc_all['2100'][(fc_all['time'] > 1989) & (fc_all['time'] < 2000)].mean()
+mean00s = fc_all['2100'][(fc_all['time'] > 1999) & (fc_all['time'] < 2010)].mean()
+mean10s = fc_all['2100'][(fc_all['time'] > 2009)].mean()
+
+plt.style.use('ggplot')
 fc_all.plot(x='time', y=["2100",'2300', "2500", '2700'], figsize = (12,6),
                 colormap = 'copper', style = '--.', linewidth = 1, markersize =4)
+plt.hlines(mean80s,1980,1989.9, color = 'grey', label = 'decadal mean')
+plt.hlines(mean90s,1990,1999.9, color = 'grey')
+plt.hlines(mean00s,2000,2009.9, color = 'grey')
+plt.hlines(mean10s,2010,2015.9, color = 'grey')
 plt.xlim([1978,2016])
 ax.tick_params('both', labelsize = 18)
 plt.xlabel('Year', fontsize = 20)
@@ -17,8 +27,8 @@ plt.ylabel('Water availability [mm]', fontsize = 20)
 ax.legend(fontsize = 16, loc = 2)
 plt.legend(title = 'Elevation [m asl]')
 plt.title('Pre-detachment total liquid water availability', fontsize = 22)
-#plt.show()
-plt.savefig('FC_all_elevations.png')
+plt.show()
+#plt.savefig('FC_all_elevations_decadalmean.png')
 
 #Print all Standard Deviations:
 

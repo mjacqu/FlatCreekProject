27acb2c04e1e87dcded3328066e2688553bc8006
Mylene Jacquemart <mjacqu@gmail.com>
2019-10-25 09:49:33 -0600

    changed colors, more reporoducible profile lines

--- FC_LongitudinalProfiles.py
+++ FC_LongitudinalProfiles.py
@@ -5,20 +5,27 @@ import matplotlib.pyplot as plt
 '''
 plot pre-, post 2013, and post 2015 longitudinal profiles along Flat Creek Glacier.
 '''
-in_file = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/TerrainAnalysis/profileview.csv'
-data = pd.read_csv(in_file, header = None, names = ['x16', 'z16', 'x12', 'z12', 'x14', 'z14'])
+in_file = '/Users/mistral/Documents/CUBoulder/Science/Sulzer/data/TerrainAnalysis/slope_profiles.csv'
+data = pd.read_csv(in_file, header = 1,
+    names = ['d12', 'x12', 'y12', 'z12', 'd14', 'x14','y14', 'z14', 'd16', 'x16', 'y16', 'z16'])
 
-fig = plt.figure(figsize=(20,8))
-plt.axis('equal')
-plt.style.use('ggplot')
-plt.plot(data.x12, data.z12, label = 'Elevation 2012')
-plt.plot(data.x14, data.z14, label = 'Elevation 2014')
-plt.plot(data.x16, data.z16, color = 'k', label = 'Elevation 2016')
-plt.xlabel('Distance [m]', fontsize = 24)
-plt.ylabel('Elevation [m]', fontsize = 24)
-plt.title('Profile view of Flat Creek failure', fontsize = 24)
+
+fig = plt.figure(figsize = (11,5))
+#plt.style.use('ggplot')
+plt.plot(data.d12, data.z12, label = 'Elevation 2012', color = 'purple')
+plt.plot(data.d14, data.z14, label = 'Elevation 2014', color = 'darkorange')
+#plt.plot(data.d15, data.z15, label = 'Elevation 2015')
+plt.plot(data.d16, data.z16, color = 'dimgray', label = 'Elevation 2016')
+plt.xlabel('Distance [m]', fontsize = 18)
+plt.ylabel('Elevation [m]', fontsize = 18)
+plt.title('Profile view of Flat Creek detachments', fontsize = 18)
+plt.ylim([1900,2650])
+plt.xlim([-20, 1650])
 ax = plt.gca()
-ax.tick_params('both', labelsize = 22)
-plt.legend(fontsize = 20)
+plt.yticks(np.arange(2000, 2800, step=200))
+ax.tick_params('both', labelsize = 16)
+plt.legend(fontsize = 16)
+plt.grid(True, color='gainsboro', linestyle='-', linewidth=1)
+plt.tight_layout()
 plt.show()
 #plt.savefig('FlatCreek_profile_view.png')
